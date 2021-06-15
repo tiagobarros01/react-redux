7174ccf3e0dcc42af32877d88f606b82ea759dcd
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-15 00:54:08 -0300

    style: Creates styles of Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import '../styles/pages/Login.css';
 import logo from '../logo.svg';
 
 function Login() {
   return (
     <div className="Container">
-      <div className="box">
+      <div className="Box">
         <img src={logo} alt="logo" />
+        <div className="Form">
+          <h2>LOGIN</h2>
           <input type="text" defaultValue="tiago@test.com" />
           <input type="password" defaultValue="123345" />
           <button onClick={() => {}}>Enter</button>
+        </div>
       </div>
     </div>
   );
